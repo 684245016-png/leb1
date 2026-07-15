70d7be9d2dbc923dcd82eee82b6d0af9b329f14e
panupong016 <684245016@webmail.npru.ac.th>
2026-07-15 15:56:54 +0700

    lab1 ข้อ2

---
+++ q2.ts
@@ -0,0 +1,7 @@
+let total =0;
+let sum=0;
+for(let day =1; day <=30; day++){
+    sum=sum+day;
+    console.log('วันที่',day,'ยอดเงิน=',sum,'บาท');
+}
+console.log('ขอแสดงความยินดี เดือนนี้คุณออมเงินได้',sum,'บาท');
